5eee31d16a33df0bceb75ece0bd38ae087cd157c
Anderson Ferreira <andersonf0018@gmail.com>
2024-08-07 16:41:18 -0300

    increase limit

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const port = process.env.PORT || 3000;
 configDotenv();
 app.use(cors());
 app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true }));
 
 app.post('/docx-to-pdf', async (req, res) => {
   try {
